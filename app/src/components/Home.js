47b43b383ec994649a93b35c653dc74afbc21288
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-17 21:54:08 -0300

    update components

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Home = () => {};
+const HomeContainer = styled.div`
+  * {
+    margin: 0;
+  }
+
+  width: 100vw;
+  height: 90vh;
+  background-color: #e2d9d9;
+
+  .home::visited {
+    color: blue;
+  }
+`;
+
+const Home = () => {
+  return (
+    <HomeContainer>
+      <h2 className="home">Home</h2>
+    </HomeContainer>
+  );
+};
 
 export default Home;
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+//components
+import Home from './Home';
 
 const Nav = styled.nav`
   width: 100vw;
   height: 10vh;
 
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  color: white;
   background-color: black;
 `;
 
 const Navbar = () => {
   return (
     <Nav>
-      <h3>Home</h3>
+      <h3>
+        <Link to="/">Home</Link>
+      </h3>
       <h3>Items</h3>
       <h3>About</h3>
     </Nav>
