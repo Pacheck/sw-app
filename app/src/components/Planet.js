b4d6dbfc590b42e4a16bccfc968952b510fc6e4b
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-20 20:34:12 -0300

    added component logic

--- app/src/components/Planet.js
+++ app/src/components/Planet.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 const Planet = ({ planetInfo }) => {
+  const films = planetInfo.films;
+  const [filmesPresentes, setFilmesPresentes] = useState([]);
+
+  useEffect(() => {
+    getFilmPresentData();
+  }, []);
+
+  //Methods
+
+  async function getFilmPresentData() {
+    films.map((film) => {
+      Axios.get(film)
+        .then((res) =>
+          setFilmesPresentes({
+            title: res.data.title,
+            episode: res.data.episode_id,
+          })
+        )
+        .then((err) => console.log('getFilmPresentData axios error ' + err));
+    });
+  }
+
+  console.log(filmesPresentes);
+
   return (
     <div>
       <h2>{planetInfo.name}</h2>
-      <h2>{planetInfo.climate}</h2>
-      <h2>{planetInfo.diameter}</h2>
       <h2>{planetInfo.terrain}</h2>
+      <h2>{planetInfo.diameter}</h2>
+      <h2>{planetInfo.climate}</h2>
     </div>
   );
 };
