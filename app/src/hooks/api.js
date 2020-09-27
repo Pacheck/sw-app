13436feac1c7cd9f0d3647f83cb7ed7ac71fba2f
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-27 05:28:16 -0300

    creating my first hook

---
+++ app/src/hooks/api.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+const API_URL = 'https://swapi.dev/api/planets';
+
+const GetAllFuckingPlanets = () => {
+  useEffect(() => {
+    getPlanetData();
+  }, []);
+
+  async function getPlanetData(params) {
+    const checkedParams = params ? params : '';
+
+    const response = await Axios.get(`${API_URL}/${checkedParams}`);
+    // const JSONres = JSON.stringify(response);
+    return response;
+  }
+
+  return getPlanetData;
+};
+
+export default GetAllFuckingPlanets;
