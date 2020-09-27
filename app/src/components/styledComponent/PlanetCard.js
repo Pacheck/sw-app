6b8237e6fa669717a9ef4c2436a5ebf430059f67
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-09-27 05:30:06 -0300

    created style to card

---
+++ app/src/components/styledComponent/PlanetCard.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const PlanetCard = styled.div`
+  width: 100%;
+  max-width: 350px;
+
+  margin-right: 10px;
+  margin-top: 20px;
+  padding: 5px;
+
+  color: #fff;
+  border: 1px solid tomato;
+
+  a {
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    color: white;
+  }
+`;
+
+export default PlanetCard;
